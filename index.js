5509aa7548bd9b9d02f2bc05fcdcb6eb4cedf04e
shawnconway <shawnway210@gmail.com>
2023-08-12 14:17:48 -0400

    modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove()
+const newHeader = document.createElement("h1")
+//newHeader.setAttribute("id","victory")
+newHeader.id="victory"
+newHeader.textContent="Shawn is the champion"
